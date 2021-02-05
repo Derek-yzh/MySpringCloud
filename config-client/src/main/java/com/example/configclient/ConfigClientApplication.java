0dd90f6c2873cba3aea1836ac9d709b2eeb7a293
Derek <806463905@qq.com>
2021-02-06 07:40:46 +0800

    配置中心的使用

--- config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -2,8 +2,11 @@ package com.example.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
@@ -17,4 +20,11 @@ public class ConfigClientApplication {
     public String test(){
         return "config-client";
     }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
 }
--- config-client/src/main/java/com/example/configclient/controler/MainController.java
+++ config-client/src/main/java/com/example/configclient/controler/MainController.java
@@ -1,9 +1,16 @@
 package com.example.configclient.controler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @Author: Derek
@@ -11,7 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description: TODO
  */
 @RestController
+@RefreshScope//配置中心自动刷新
 public class MainController {
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @Value("${config.info}")
     String info;
@@ -20,5 +31,14 @@ public class MainController {
     public String test(){
         return info;
     }
+    @GetMapping("/refresh")
+    public String refresh(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE,"application/json");
+        HttpEntity<String> request = new HttpEntity<>(headers);
+        restTemplate.postForEntity("http://localhost:8999/actuator/refresh",request,String.class);
+        return "刷新成功";
+    }
+
 
 }
