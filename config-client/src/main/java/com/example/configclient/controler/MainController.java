23d813b85cb466e45aa3f050c3a57a1dee008d7f
Derek <806463905@qq.com>
2021-02-05 16:22:56 +0800

    配置中心的使用

--- config-client/src/main/java/com/example/configclient/controler/MainController.java
+++ config-client/src/main/java/com/example/configclient/controler/MainController.java
@@ -3,13 +3,14 @@ package com.example.configclient.controler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author: Derek
  * @DateTime: 2021/1/5 14:12
  * @Description: TODO
  */
-@Controller
+@RestController
 public class MainController {
 
     @Value("${config.info}")
